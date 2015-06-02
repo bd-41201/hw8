51186adfab391b26d87cc6f293fa7de46c0e1f17
Tom Besio <thomas.besio@gmail.com>
2015-06-02 11:19:23 -0500

    Fit unpruned tree

--- hw8_script.r
+++ hw8_script.r
@@ -1,3 +1,23 @@
 ### HW 8 Script
 # Using project data (loan stats from Lending Club) to dig into trees and random forests
 
+## read in the data
+loanstats <- read.csv("loanstats_trimmed.csv")
+# This is the "cleaned" data from the final project so some columns have been removed
+
+## Part 1 - Fit a tree using CART and then prune. Plot the results for each step.
+library(tree)
+# We use the tree library for this
+
+# Right now we must remove the addr_state field because the tree function cannot
+# handle factors with more than 32 levels. We'll explore some methods around this
+# later.
+loan.tree <- tree(int_rate ~.-addr_state, data=loanstats)
+
+# Plot to see the results
+# png('loanstats_tree_unpruned.png')
+par(mfrow=c(1,1))
+plot(loan.tree, col=8)
+text(loan.tree, digits=2,pretty=TRUE)
+title("Loan Rates Tree - Unpruned")
+# dev.off()
